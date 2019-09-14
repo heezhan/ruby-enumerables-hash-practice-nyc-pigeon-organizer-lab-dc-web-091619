4a9abe75d90cd2391be8bfc754844bd1f2557f11
Heejae Han <hhan1194@gmail.com>
2019-09-14 15:10:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -35,6 +35,17 @@ def nyc_pigeon_organizer (data)
     end 
   end 
   
-    
+    data.each do |attribute, stat|
+    if attribute == :color
+      stat.each do |sub_attribute, pigeon|
+        pigeon.each do |name|
+          final_keys.each do |bird_name|
+          final[name][:lives] << sub_attribute.to_s if name == bird_name
+          end 
+        end 
+      end 
+    end 
+  end 
+  
   return final 
 end 
